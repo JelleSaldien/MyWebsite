8a4a17a5576b3a93204981813324521d0ea86af7
jsaldien <jelle.saldien@ugent.be>
2023-02-26 15:28:47 +0100

    update on resize and image link

--- files/js/googlechartimport.js
+++ files/js/googlechartimport.js
@@ -3,6 +3,10 @@
 google.charts.load('current', {'packages':['bar']});
 google.charts.setOnLoadCallback(drawChart);
 
+var chart;
+var options;
+var data;
+
 function drawChart() {
   var queryString = encodeURIComponent('SELECT A, C, D'); // Replace A and B with the column headers in your spreadsheet
   var query = new google.visualization.Query(
@@ -11,20 +15,20 @@ function drawChart() {
 }
 
 function handleQueryResponse(response) {
-  var data = response.getDataTable();
-  var options = {
+  data = response.getDataTable();
+  options = {
     chart: {
       title: 'The Kahoot EmTech Quiz Scores',
     },
     bars: 'horizontal', // Change to 'vertical' if you want vertical bars
     stacked: true,
   };
-  var chart = new google.charts.Bar(document.getElementById('chart_div'));
- 
-  function resizeChart() {
-    chart.draw(data, options);
-  }
+  chart = new google.charts.Bar(document.getElementById('chart_div'));
+  chart.draw(data, options);
+  
+}
 
-  window.addEventListener('resize', resizeChart);
-  resizeChart();
+function resizeChart() {
+  chart = new google.charts.Bar(document.getElementById('chart_div'));
+  chart.draw(data, options);
 }
