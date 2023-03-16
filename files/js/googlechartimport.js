57dd22a9e4fcd806630b995d45b108856ad2bcb6
jsaldien <jelle.saldien@ugent.be>
2023-03-16 22:34:36 +0100

    update kahoot scores

--- files/js/googlechartimport.js
+++ files/js/googlechartimport.js
@@ -8,7 +8,7 @@ var options;
 var data;
 
 function drawChart() {
-  var queryString = encodeURIComponent('SELECT A, C, D, E'); // Replace A and B with the column headers in your spreadsheet
+  var queryString = encodeURIComponent('SELECT A, C, D, E, F'); // Replace A and B with the column headers in your spreadsheet
   var query = new google.visualization.Query(
       'https://docs.google.com/spreadsheets/d/1O1U5HUjsVnCu2f-DhbEjVxQD9apvfOojQ1IuJ4PS2Tw/gviz/tq?gid=465013469&headers=1&tq=' + queryString);
   query.send(handleQueryResponse);
